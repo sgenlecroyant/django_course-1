b7bce26790478ba37a99174d250b59b47eaf107f
Edgar Eldy <ed.eldy21@live.com>
2022-11-22 10:04:34 +0200

    Install app

--- django_course/settings.py
+++ django_course/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,7 @@ WSGI_APPLICATION = 'django_course.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
