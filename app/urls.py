7d83eeb4d360d6fda2530fe1c0f1e486733e8643
Edgar Eldy <ed.eldy21@live.com>
2022-11-25 13:36:18 +0200

    Add categories urls

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,10 @@ from app import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home.index, name='home')
+    path('', views.home.index, name='home'),
+    path('categories/', views.categories.index, name='categories_index'),
+    path('categories/create', views.categories.create, name='categories_create'),
+    path('categories/store', views.categories.store, name='categories_store'),
+    path('categories/edit/<int:id>', views.categories.edit, name='categories_edit'),
+    path('categories/delete/<int:id>', views.categories.delete, name='categories_delete'),
 ]
